899736ad136ac7587d25ebc1e0d7130e658dd08b
Marco Tan <90207324+marcotan004@users.noreply.github.com>
2023-10-10 14:26:01 -0400

    added comments

--- lab2/analysis.py
+++ lab2/analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 def processData(file_name):
+    ''' data --> vectors '''
     file = open(file_name, 'r')
     processing = False
     x, y, data = [], [], []
@@ -22,9 +23,11 @@ def processData(file_name):
     return x, y
 
 def fit_gaussian(x, H, A, mu, sigma):
+    ''' gaussian fit '''
     return A*np.exp(-(x-mu)**2/(2*sigma**2)) + H
 
-def get_x_val(energy):
+def get_x_val(energy): 
+    '''get the x index that matches the energy'''
     return int((energy-1.36)/(0.243))
 
 def get_gaussian(start_energy, end_energy, x, y):
@@ -39,14 +42,17 @@ def get_gaussian(start_energy, end_energy, x, y):
     return get_mu_sigma(curve_fit(fit_gaussian, x, y, p0=[min(y), max(y), mean, sigma]))
 
 def get_mu_sigma(results):
+    ''' get the mean and sigma values for the gaussian from the curve fit '''
     return results[0][2], np.diag(results[1])[2]
 
 def plot_gaussian(start_energy, end_energy, x, y, params):
+    ''' plot the gaussian '''
     fit_x = np.linspace(np.min(x_hist),np.max(x_hist),500)
     plt.plot(fit_x, fit_gaussian(fix_x, *params), 'r.:', label='gaussian fit')
     plt.legend()
 
 def get_moment(factor):
+    ''' calculate the moment of inertia '''
     h = 4.135*(10**-15)*(10**-3) #keV * s
 
     return((2*factor)/(h**2))
@@ -54,22 +60,23 @@ def get_moment(factor):
 if __name__ == '__main__':
     data_file = 'Ho166_9-28-23.IEC'
     
+    # get the and transform the vectors
     x, y = processData(data_file)
     x = np.array(x)
     for i, item in enumerate(y):
         y[i] = int(item)
+    y = np.array(y) # counts
+    x = (0.243*x) + 1.36 # keV
 
-    y = np.array(y)
-    x = (0.243*x) + 1.36 # multiply by 1000 since in keV
-
+    # calculate gaussian curve fits for corresponding gamma rays
     intervals = [[361.8, 367.7], [276.6, 281.81], [180.9, 185.92], [78, 81.5]]
     intervals.reverse()
-
     results = []
     for i in intervals:
         r = get_gaussian(i[0], i[1], x, y)
         results.append([r[0], r[1]])
     
+    # consolidate results into mean and standard deviation arrays
     levels, std = [0] * len(intervals), [0] * len(intervals)
     levels[0] = results[0][0]
     std[0] = results[0][1]
@@ -77,9 +84,9 @@ if __name__ == '__main__':
         levels[i] = levels[i-1] + results[i][0]
         std[i] = std[i-1] + results[i][1]
 
+    # match levels to factors from red book and get moment of inertia
     levels,std = np.array(levels),np.array(std)
     factors = np.array([6, 20, 42, 72])
-
     print('moment: {0} +- {1}'.format(get_moment(factors), get_moment(std)))
     print('level/factors: {0}'.format(levels/factors))
     
