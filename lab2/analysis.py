2e67a5176f5daff597a74aae4d362ba85f69bf6d
Marco Tan <90207324+marcotan004@users.noreply.github.com>
2023-10-12 11:00:51 -0400

    beta calculation

--- lab2/analysis.py
+++ lab2/analysis.py
@@ -54,18 +54,47 @@ def plot_gaussian(start_energy, end_energy, x, y, params):
 def get_moment(energy, factor):
     ''' calculate the moment of inertia '''
     h = 6.582*(10**-16) #eV * s
-    energy = energy*1000 # keV -> eV
+    energy = energy*1000# keV -> eV
 
-    return (h**2/(2*energy))*factor
+    return (h**2/(2*energy))*factor # (ev * s)**2 / (eV) = eV * (s ** 2) 
+
+def get_moment_uncertainty(std, energy, factor):
+    h = 6.582*(10**-16) #eV * s
+    return (factor)*(h**2)*(1/2)*(std/energy)
 
 def expected_energy(moment, factor):
     h = 6.582*(10**-16) 
 
     return ((h**2)/(2*(moment))) * factor
 
+def phi_rigid(M, R, phi):
+    ''' calculate beta of the rigid body model '''
+    # phi in eV * (s ** 2) 
+    # convert to kg * m^2
+    conv = 1.602 * (10**-19)
+    phi = phi * conv
+
+    # calculate beta
+    numerator = 5*phi
+    denominator = 0.31 * 2 * M * R
+
+    return (numerator/denominator) - 1 # returns beta
+
+def phi_fluid(M, R, phi):
+    # phi in eV * (s ** 2) 
+    # convert to kg * m^2
+    conv = 1.602 * (10**-19)
+    phi = phi * conv
+
+    # calculate beta
+    numerator = 8 * phi * math.pi 
+    denominator = 9 * M * (R ** 2)
+    return math.sqrt(numerator/denominator)
+
 if __name__ == '__main__':
     data_file = 'Ho166_9-28-23.IEC'
-    
+    plot = False # plot option
+
     # get the and transform the vectors
     x, y = processData(data_file)
     x = np.array(x)
@@ -93,14 +122,24 @@ if __name__ == '__main__':
     # match levels to factors from red book and get moment of inertia
     levels,std = np.array(levels),np.array(std)
     factors = np.array([6, 20, 42, 72])
-    print(levels)
-    print(factors)
-    print('moment: {0} +- {1}'.format(get_moment(levels, factors), get_moment(levels, std)))
+    moments = get_moment(levels, factors)
+    uncert = get_moment_uncertainty(std, levels, factors)
+
+    print('moment of inertia (eV * s**2): {0} +- {1}'.format(moments, uncert))
+    print('mean moment of inertia: {0}'.format(np.mean(moments)))
     print('level/factors: {0}'.format(levels/factors))
-    print('expected energy {0}'.format(expected_energy(get_moment(levels, factors)[0], 6)))
     
-    ax = plt.gca()
-    ax.set_xlim(min(x), 2000)
-    ax.set_ylim(min(y), 50000)
-    plt.bar(x,y)
-    plt.show()
+    # calculate beta values using mean phi
+    M = 165.93228 * (1.6605 * 10**-27) # atomic mass of Ho-166 (kg)
+    R = (1.2 *(10 ** -15)) * (166 ** (1/3))
+    phi = np.mean(moments)
+    beta_rigid = round(phi_rigid(M, R, phi), 2)
+    beta_fluid = round(phi_fluid(M, R, phi), 2)
+    print('Beta of rigid body model: {0} | Beta of fluid model: {1}'.format(beta_rigid, beta_fluid))
+
+    if plot:
+        ax = plt.gca()
+        ax.set_xlim(min(x), 2000)
+        ax.set_ylim(min(y), 50000)
+        plt.bar(x,y)
+        plt.show()
