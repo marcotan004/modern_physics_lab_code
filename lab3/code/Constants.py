3bf88287a9dd6be78a53159301f197d9557342ce
Marco Tan <90207324+marcotan004@users.noreply.github.com>
2023-11-10 14:29:15 -0500

    add constants

---
+++ lab3/code/Constants.py
@@ -0,0 +1,11 @@
+dE_ALUM_E_5490 = 1.59 * (10**2) # keV/micron
+dE_ALUM_N_5490 = 1.106 * (10**-1) # keV/micron
+dE_HAV_E_5490 =  3.613 * (10**2) # keV/micron
+dE_HAV_N_5490 =  2.842 * (10**-1) # kev/micron
+dE_HAV_E_3180 = 4.911 * (10**2)
+dE_HAV_N_3180 = 4.507 * (10**-1)
+dE_ALUM_E_3180 = 2.113 * (10**2)
+dE_ALUM_N_3180 = 1.770 * (10**-1)
+CALIB_SLOPE = 3.883
+CALIB_INT = 37.4
+AM_PEAK = 1400
